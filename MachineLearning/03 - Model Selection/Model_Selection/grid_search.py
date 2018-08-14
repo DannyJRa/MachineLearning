0cca4aa69ec366a09fa9cf61bf3122e3c818fb64
DannyJRa <danny_zurich@outlook.com>
2018-08-14 22:36:13 +0200

    ml update

--- MachineLearning/03 - Model Selection/Model_Selection/grid_search.py
+++ MachineLearning/03 - Model Selection/Model_Selection/grid_search.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv('C:/OneDrive/7_DataScience/21_EC2/01_Github/MachineLearning/MachineLearning/03 - Model Selection/Model_Selection/Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
