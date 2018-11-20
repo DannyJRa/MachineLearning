0360ccb92154dc36406469293f7be3d6f5d90084
DannyJRa <danny_zurich@outlook.com>
2018-11-20 23:34:08 +0100

    test again

--- MachineLearning/03 - Model Selection/Model_Selection/grid_search.py
+++ MachineLearning/03 - Model Selection/Model_Selection/grid_search.py
@@ -40,7 +40,7 @@ accuracies.std()
 
 #Test compute time
 #Test compute time V2
-#Test compute time V23
+#Test compute time V234
 ## Applying Grid Search to find the best model and the best parameters
 from sklearn.model_selection import GridSearchCV
 parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
